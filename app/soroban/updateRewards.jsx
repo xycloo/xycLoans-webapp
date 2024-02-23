04f9c2fcc5e55b1ee735b25fcf4316f9712abf5f
heytdep <tommasodeponti@zohomail.eu>
2024-02-23 18:10:20 +0100

    adapt updaterewards from home for production messaging

--- app/soroban/updateRewards.jsx
+++ app/soroban/updateRewards.jsx
@@ -24,18 +24,18 @@ export default async function UpdateRewards(params) {
         const loadingMessage = "Updating matured fee rewards"
         const message = "Updated matured fee rewards"
         
-        if (!params.fromHome) {
-            router.push(`?show=${loadingMessage}`)
+        //if (!params.fromHome) {
+        router.push(`?show=${loadingMessage}`)
 
-            try {
-                await publishTx(publicKey, contract_call);
-                router.push(`?success=${message}`)
-                router.refresh()
-            } catch (e) {
-                router.push(`?error=${e}`)
-                router.refresh()
+        try {
+            await publishTx(publicKey, contract_call);
+            router.push(`?success=${message}`)
+            router.refresh()
+        } catch (e) {
+            router.push(`?error=${e}`)
+            router.refresh()
             }
-        } else if (params.fromHome) {
+        /*} else if (params.fromHome) {
             router.push(`home/?show=${loadingMessage}`)
 
             try {
@@ -46,7 +46,7 @@ export default async function UpdateRewards(params) {
                 router.push(`home/?error=${e}`)
                 router.refresh()
             }
-        }
+        }*/
     } return (
         <button onClick={handleUpdateRewards} className="rounded-lg w-40 m-auto bg-gradient-to-r from-[#6366f1] to-[#9333ea] hover:bg-gradient-to-r hover:from-[#4f46e5] hover:to-[#7e22ce] transition duration-900 ease-in-out text-white font-normal mx-1 shadow-md">
         <span className="text-sm w-max mx-auto">
